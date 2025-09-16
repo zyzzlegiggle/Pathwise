cb57faf7b7d6d0dd9b0b358668d7008db97f586d
zyzzlegiggle <112403713+zyzzlegiggle@users.noreply.github.com>
2025-09-16 09:16:51 +0800

    3

--- career-sim/eslint.config.mjs
+++ career-sim/eslint.config.mjs
@@ -1,21 +1,18 @@
-// eslint.config.mjs
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({ baseDirectory: __dirname });
-
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-       '@typescript-eslint/no-empty-object-type': ['error', { allowObjectTypes: 'always' }],
+      "@typescript-eslint/no-empty-object-type": ["error", { allowObjectTypes: "always" }],
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        {
+          "ts-expect-error": "allow-with-description",
+          "ts-ignore": false, // set to true if you want to allow it
+          "ts-nocheck": true,
+          "ts-check": true,
+        },
+      ],
     },
   },
 ];
-
-export default eslintConfig;
